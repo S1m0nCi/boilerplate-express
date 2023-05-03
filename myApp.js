5e751f92314c38aa89479dc0c565300cbac82763
SimonC <115393581+S1m0nCi@users.noreply.github.com>
2023-05-03 17:04:55 +0100

    Task 5

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 let express = require('express'); // importing the express module.
 let app = express();
 
+const PATH = __dirname + '/public'
+app.use(PATH, express.static())
+
 // Without using arrow notation or async, below:
 app.get('/', function (req, res) {
     console.log("Hello Express")
