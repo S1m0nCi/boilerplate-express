257f3d1e219c9c7c09d4e3d168f07ac29e248efa
SimonC <115393581+S1m0nCi@users.noreply.github.com>
2023-05-28 16:33:51 +0100

    Debugging

--- myApp.js
+++ myApp.js
@@ -1,20 +1,23 @@
-let express = require('express'); // importing the express module.
-let app = express();
+const express = require('express'); // importing the express module.
+const app = express();
 
-const PATH = __dirname + '/public'
-app.use(PATH, express.static())
+app.use('/public', express.static(__dirname + '/public'));
 
 // Without using arrow notation or async, below:
 app.get('/', function (req, res) {
-    console.log("Hello Express")
-    res.sendFile(__dirname + '/views/index.html')   
-})
+    console.log("Hello Express");
+    res.sendFile(__dirname + '/views/index.html');   
+});
 
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+});
 
+/*
 app.get('/json', (req, res) => {
     res.json({'message': 'Hello json'})
 })
-
+*/
 
 
 
